979b0f2c433bf0d05e470c3650292c30ab42d8c6
Marco Hernandez <ma.hl@outlook.com>
2021-08-11 12:23:46 -0600

    Mock Task & Task interface

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -1,9 +1,11 @@
-export const TASKS = [
+import { Task } from './Task'
+
+export const TASKS : Task [] = [
     {
       id: 1,
       text: 'Doctors Appointment',
       day: 'May 5th at 2:30pm',
-      reminder: true,
+      reminder: true
     },
     {
       id: 2,
